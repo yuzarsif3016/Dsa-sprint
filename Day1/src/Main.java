2d92451967b17ae3e2822047c0b1a6f62ac025f3
yuzarsif3016 <yusufpathan3016@gmail.com>
2025-07-26 12:23:33 +0530

    first commit

---
+++ Day1/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        
+        System.out.println(list);
+    }
+}
