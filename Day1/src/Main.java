2749ab960fa9b51848853272e893e450f635a1bf
yuzarsif3016 <yusufpathan3016@gmail.com>
2025-07-26 12:54:36 +0530

    Collection framework basics-arraylist and linkedlist

--- Day1/src/Main.java
+++ Day1/src/Main.java
@@ -1,13 +1,29 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+//        ArrayList<Integer> list = new ArrayList<>();
+//
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//        list.add(40);
+//
+//        System.out.println(list);
+//
+//        for(var item: list){
+//            System.out.println("element is " + item);
+//        }
+
+        LinkedList<Integer> list = new LinkedList<>();
 
         list.add(10);
-        list.add(20);
+        list.add(23);
         list.add(30);
-        list.add(40);
+        list.add(22);
+        list.add(null);
+        list.addFirst(12);
 
         System.out.println(list);
     }
